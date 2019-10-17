55133a8933bcafb0413a8d6b5899d22ac87712f2
Jonathan Chukinas (work computer) <jchukina@its.jnj.com>
2019-10-17 12:11:41 -0400

    fix custom errors

--- mentormatch/main/exceptions.py
+++ mentormatch/main/exceptions.py
@@ -12,20 +12,5 @@ class MentormatchError(Exception):
     pass
 
 
-class MentormatchFileError(MentormatchError):
-    """
-    Raise this for any errors related to the excel file itself.
-    Examples:
-        wrong file extension
-        file missing
-        missing worksheet
-    """
-    pass
-
-
-class UninitializedError(Exception):
-    pass
-
-
-class Uninitialized(MentormatchError):
+class UninitializedError(MentormatchError):
     pass
