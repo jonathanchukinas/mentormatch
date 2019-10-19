a5f586c704acfcc6ae7561314fc632cd2d60e9e5
Jonathan Chukinas (home computer) <chukinas@gmail.com>
2019-10-19 11:07:03 -0400

    restructure matching dir

--- mentormatch/matching/preferred_matching.py
+++ mentormatch/matching/preferred_matching.py
@@ -25,34 +25,7 @@ def preferred_matching():
     #           If mentee has a priority level, increment, add to end of deque
     #           Else: Let this mentee fall out of the process. She'll get priority during random pairings.
     #       Repeat.
-    #
-    #
-    #
-    # TODO pseudocode for random pairing
-    #   give priority to:
-    #       1) Those marked with priority
-    #       2) Those who chose preferred mentors but didn't receive one.
-    #
-    #
-    #
-    # TODO pseudocode
-    #   This serves mostly (only?) to compare one potentially pairing with another.
-    #   Comparisons are based on:
-    #   Ties are broken in the following order (starting with the first):
-    #       1 - Mentor preferences (site, gender). A mentee who has more "yes"es break the tie.
-    #       2 - Level difference: A smaller level difference wins.
-    #       3 - Rank Order: Whichever mentee ranked this mentor higher wins.
-    #       4 - Count of preferred wwids: The mentee who listed more preferred wwids wins.
-    #       5 - History - the mentee who's been rejected the most or had the least number of mentors wins
-    #       6 - Priority - mentees who were rejected last year fall into this category.
-    #               These are the ones we want badly to match up this year.
-    #               This one has the potential to move up the tiebreaker order.
-    #               If, for example, we arrive at the end and there are 'priority' mentees
-    #       7 - First applied - probably will only rarely come up, but there needs to be some final tie breaker...
-    #   There are other factors I brainstormed, but they won't be used during the preferred matching phase.
-    #       Random vs. Preferred Pairing -- not used in preferred matching
-    #       Does the Mentee want a random pairing at all? -- not used in preferred matching
-    #       Mentee preferences (site, gender)  -- not used in preferred matching
+
 
     # Randomize mentees with preferred wwids into a queue
     mentees = self.__mentees
@@ -86,7 +59,3 @@ def preferred_matching():
         mentee.could_not_find_a_match()
         if mentee.still_has_chances():
             mentee_queue.append(mentee)
-
-
-def random_matching():
-    pass
