cb403684f457bcf5eea0942a42317245d7bcb3d6
Jonathan Chukinas (home computer) <chukinas@gmail.com>
2019-10-19 10:54:59 -0400

    v0.1
    initial commit
    
    restructure matching dir
    
    restructure matching dir
    
    skeleton of PreferredMatching class
    
    cleaned up imports with __init__ files
    
    in the middle of setting up __init__ files
    
    same as below
    
    all tests working
    
    removed extraneous funcs from api.py b/c covered by autosetup
    
    remove extraneous hash func from preferred matching
    
    i just realized that I can't store sequences in pandas... I think I'll try TinyDB
    
    initial commit
    
    just experimented with saving and deleting the json file
    
    laid out structure
    
    3 workbook tests
    
    get_header_row_number added
    
    wip drop_dups
    
    import_worksheet_to_db. still need to implement validation
    
    experiment with modifying all documents
    
    initial commit
    
    create MentoringField
    
    convert field_schema to FieldPatterns
    
    implement fuzzytable
    
    delete config.groups
    
    add test section to pyproject.toml
    
    about to implement AllApplicants / Group / SingleApplicant class structure
    
    AllApplicants drafted
    
    GroupApplicants drafted
    
    SingleApplicant drafted
    
    Mentee drafted
    
    Mentor draftedd
    
    Mentee drafted
    
    removed matching dir
    
    assign_to_preferred_mentor looking good
    
    fieldschema and first draft of mentee application
    
    wip tiebreakers
    
    clean up all_applicants
    
    clean up mentors_and_mentees
    
    about to implement enumeration in fuzzytable
    
    need to add fuzzytable StringChoiceMulti
    
    wip
    
    wip
    
    wip
    
    writing allapplicants test
    
    need fuzzytable v0.19 (fieldpattern case_sensitive param)
    
    AllApplicants successfully run
    
    wip
    
    homesunday
    
    random match pseudocode
    
    20191223 MatchingAlgorithm class
    
    cleanup, remove superseded code
    
    tests pass
    
    wip
    
    UU beats ZZ on relative preferred mentor rank
    
    wip
    
    tests cover favored
    
    wip error handling
    
    94% cov
    
    95% cov
    
    97% cov
    
    98% cov
    
    99% cov
    
    fieldschema.toml
    
    wip factory & DI
    
    api.py done
    
    wip20200120
    
    wip
    
    create IPairRanker
    
    created several Ranker classes
    
    wip dependency inversion: pair comparisons
    
    testing relative imports
    
    applicants init file
    
    rankers init file
    
    PairRankerBuilder
    
    wip
    
    ranker_preferred_mentee_initialization
    
    wip
    
    PairRankerSkillsAndFunctions
    
    ranker_random
    
    MinMax
    
    PairRankerLocationAndGender
    
    clean up pair ranker concretes
    
    wip
    
    wpi
    
    PairRankerContextMgr
    
    pair_ranker_context_manager
    
    compatibility_checker
    
    applicant
    
    pair.py
    
    ranker
    
    ranker
    
    implementation
    
    setup
    
    setup
    
    applicant_factory
    
    ApplicantCollection
    
    Initializer
    
    initializer init
    
    _get_initializer
    
    Initializer
    
    move api into separate dir
    
    MatchSummary
    
    add mentor and mentee test people:
    
    smoke test
    
    pytest troubleshooting
    
    test passes
    
    wip
    
    91% test cov
    
    92% test cov
    
    93% test cov
    
    93% test cov
    
    Applicant: rename ranker param to sorter
    
    wip
    
    94% test cov
    
    95% test cov
    
    96% test cov
    
    wip
    
    wip
    
    testing
    
    troubleshooting tests
    
    wip
    
    100% pass, 95% cov
    
    made Applicant an ABC again
    
    export
    
    tests pass 100%, cov 98%
    
    about to remove IPair
    
    just removed IPair by using __futures__ annotations and TYPE_CHECKING
    
    implement TYPE_CHECKING in more plcaes
    
    wip
    
    add coverage exclusions to tox.ini
    
    about to implement importer excel
    
    importer_implementation_excel
    
    wip fieldschema
    
    import
    mostly runs now
    
    seems complete
    
    tests pass
    
    v1.0

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,49 @@
-from pathlib import Path
+import toml
 import pytest
-import openpyxl
-from mentormatch.worksheet.worksheet import Worksheet
+from pathlib import Path
+from .random_applicant_generator import RandomApplicantGenerator
+from datetime import datetime
 
 
-test_mentormatch_xslx_path = Path(__file__).parent / "test_mentormatch.xlsx"
+_test_files_dir = Path(__file__).parent / "files"
+mentor_count =10
+mentee_count = 2 * mentor_count
+now = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+
+@pytest.fixture(scope='function')
+def mentors():
+    _dict = toml.load(_test_files_dir / 'mentors.toml')['mentors']
+    return _dict
+
+
+@pytest.fixture(scope='function')
+def mentees():
+    _dict = toml.load(_test_files_dir / 'mentees.toml')['mentees']
+    return _dict
+
+
+@pytest.fixture(scope='function')
+def lots_of_applicants():
+    applicant_generator = RandomApplicantGenerator()
+    applicant_generator.build_mentors(mentor_count)
+    applicant_generator.build_mentees(mentee_count)
+    return applicant_generator.applicants_dicts
+
+
+@pytest.fixture(scope='function')
+def three_perfect_applicants():
+    applicants = toml.load(_test_files_dir / 'perfect_matches.toml')
+    return applicants
 
 
 @pytest.fixture(scope='session')
-def fixture_path():
-    return test_mentormatch_xslx_path
+def test_files_dir():
+    return _test_files_dir
 
 
-@pytest.fixture(scope='session')
-def fixture_get_ws():
-    def get_ws(worksheet_name: str, autosetup=False):
-        return Worksheet(test_mentormatch_xslx_path, worksheet_name, autosetup=autosetup)
-    return get_ws
+@pytest.fixture(scope='function')
+def home_dir():
+    path = Path.home() / '.mentormatch' / f'mentormatch_{now}'
+    # path.mkdir()
+    return path
--- tests/experiments/experiment_convert_to_string.py
+++
@@ -1,6 +0,0 @@
-validation_function = str
-
-val_before = 1  # an integer
-print(val_before, type(val_before))
-val_after = validation_function(val_before)  # a string
-print(val_after, type(val_after))
