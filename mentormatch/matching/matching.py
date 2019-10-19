ddaa418594fd243e7680cfdb3fb930d42dab8ee3
Jonathan Chukinas (home computer) <chukinas@gmail.com>
2019-10-19 10:54:59 -0400

    initial commit

--- mentormatch/matching/matching.py
+++ mentormatch/matching/matching.py
@@ -1,4 +1,40 @@
 def preferred_matching():
+    # TODO pseudocode - high level
+    #   "Randomly" order the mentees (in a repeatable manner)
+    #       Hash a wwid+year string
+    #       Create a deque container containing just those mentee who listed preferred mentors
+    #   For each mentee, check their list of preferred mentors
+    #       add a new column: tentative_mentors.
+    #       Convert preferred wwids to id (this involves checking to make sure that wwid actually exists"
+    #       For each preferred mentor (id):
+    #           make sure the mentee doesn't violate any of the mentor's deal breakers
+    #               (such as mentee's site not being on the mentor's yes or maybe site lists)
+    #   For each mentor, create a column `tentative_mentees`
+    #   Take first mentee off the deque:
+    #       Assign them to their most preferred mentor.
+    #       Sort all mentees assigned to this mentor by tie breaker (maybe use another deque?)
+    #       If mentor is still under capacity, stop. Move to next mentee in deque
+    #       Elif that takes the mentor above capacity, take the mentee who scores lowest on the tie breaker
+    #           If that mentee is the same as the one we took of the deque:
+    #               repeat the above with their next preferred mentor
+    #           elif the removed mentee is a different one, add mentee to end of deque and:
+    #               if mentee has a priority level, increment their priority
+    #               else: modify this mentee's tentative_mentors list, removing this and higher-ranked mentors.
+    #                   (There's no way they can now win these mentors)
+    #       If mentee get to end of their tentative mentors (they' couldn't be successfully matched):
+    #           If mentee has a priority level, increment, add to end of deque
+    #           Else: Let this mentee fall out of the process. She'll get priority during random pairings.
+    #       Repeat.
+    #
+    #
+    #
+    # TODO pseudocode for random pairing
+    #   give priority to:
+    #       1) Those marked with priority
+    #       2) Those who chose preferred mentors but didn't receive one.
+    #
+    #
+    #
     # TODO pseudocode
     #   This serves mostly (only?) to compare one potentially pairing with another.
     #   Comparisons are based on:
