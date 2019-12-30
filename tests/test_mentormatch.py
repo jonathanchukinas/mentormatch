f498909ff1cb08353f8dcfe59fe9dc1c06cbe11e
Jonathan Chukinas (home computer) <chukinas@gmail.com>
2019-12-30 17:14:27 -0500

    94% cov

--- tests/test_mentormatch.py
+++ tests/test_mentormatch.py
@@ -2,6 +2,8 @@ from mentormatch.applicants import AllApplicants
 from mentormatch.main.api import main
 import pytest
 import toml
+from mentormatch.main import exceptions
+import mentormatch
 
 
 # def test_allapplicants(applications_path):
@@ -23,3 +25,10 @@ def test_pairs(pairs_path):
     assert 432198765 in pairs[234567891]
     assert 765432198 in pairs[456789123]
 
+
+@pytest.mark.parametrize("filename", ['missing_worksheet.xlsx', 'wrong_row.xlsx'])
+def test_exceptions(filename, test_files_dir):
+
+    test_file_path = test_files_dir / filename
+    with pytest.raises(exceptions.MentormatchError):
+        mentormatch.applicants.AllApplicants(test_file_path)
