5b65c5a5f30390e60804973be26bc208958b22c9
Jonathan Chukinas (home computer) <chukinas@gmail.com>
2020-02-02 16:30:09 -0500

    wip fieldschema

--- mentormatch/importer/importer_implementation_excel_schema.py
+++ mentormatch/importer/importer_implementation_excel_schema.py
@@ -3,101 +3,105 @@ should contain."""
 from fuzzytable import FieldPattern, cellpatterns as cp
 import toml
 from pathlib import Path
+from mentormatch.utils import ApplicantType
+from mentormatch.utils import exceptions
 
 
 # Get fieldschema from toml
 _application_schema_path = Path(__file__).parent.parent.parent / "application_schema.toml"
 _application_schema = toml.load(_application_schema_path)
+_survey_question_aliases = dict(_application_schema['survey_questions']['with_alias']).update({
+    fieldname: None
+    for fieldname in _application_schema['survey_questions']['no_alias']
+})
+_selections = _application_schema['selections']
 
 
 class MentoringField(FieldPattern):
     # Wrapper around fuzzytable FieldPattern class
 
-    # TODO why is this a class attr?  Can it be moved external to the class?
-    survey_question_aliases = dict(_application_schema['survey_questions']['with_alias']).update({
-        fieldname: None
-        for fieldname in _application_schema['survey_questions']['no_alias']
-    })
-
     def __init__(self, name, cellpattern=None, mentor_only=False, mentee_only=False):
-        self.mentor_only = mentor_only
-        self.mentee_only = mentee_only
-        alias = self.get_alias(name)
+        self._applicable_to = {
+            ApplicantType.MENTOR: not mentee_only,
+            ApplicantType.MENTEE: not mentor_only
+        }
         super().__init__(
             name=name,
-            alias=alias,
+            alias=_survey_question_aliases[name],
             mode='approx',
             min_ratio=0.5,
             cellpattern=cp.String if cellpattern is None else cellpattern,
             case_sensitive=False,
         )
 
-    @classmethod
-    def get_alias(cls, field_name):
-        try:
-            return cls.survey_question_aliases.pop(field_name)
-        except KeyError:
-            raise exceptions.MissingFieldschemaError('survey_questions', field_name)  # TODO make sure these generate an error
-
-    @classmethod
-    def check_for_unused_toml_fields(cls):
-        unused_fieldnames = list(cls.survey_question_aliases.keys())
-        if len(unused_fieldnames) > 0:
-            raise exceptions.UnusedFieldschemaError('survey_questions', unused_fieldnames)
+    def applicable_to(self, _type: ApplicantType) -> bool:
+        return self._applicable_to[_type]
 
 
-class Selections:
-
-    def __init__(self):
-        self._selections = dict(toml_schema['selections'])
-        self._used_selections = set()
-
-    def __getattr__(self, item):
-        self._used_selections.add(item)
-        try:
-            self._selections.pop(item)
-        except KeyError:
-            raise exceptions.MissingFieldschemaError('selections', item)  # TODO make sure these generate an error
-
-    def check_for_unused_toml_fields(self):  # TODO make sure these generate an error
-        unused_selections = list(set(self._selections.keys()) - set(self._used_selections))
-        if len(unused_selections) > 0:
-            raise exceptions.UnusedFieldschemaError('survey_questions', unused_selections)
+_fieldschemas = [
 
 
-selections = Selections()
+    # BASIC BIO
+    MentoringField("last_name"),
+    MentoringField("first_name"),
+    MentoringField("wwid", cp.Integer),
+    MentoringField("nickname"),
+    MentoringField("email_given"),
+    MentoringField("job_title"),
 
 
-MF = MentoringField
-_fieldschema = [
-    MF("first_name"),
-    MF("nickname"),
-    MF("last_name"),
-    MF("gender", cp.StringChoice(
-        choices=selections.genders,
+    # EXPERIENCE
+    MentoringField("position_level", cp.Digit),
+    MentoringField("years_total", cp.Float),
+    # MentoringField("years_jnj", cp.Float),
+
+
+    # YES/NO/MAYBE
+    # (appended down below)
+    MentoringField("gender", cp.StringChoice(
+        choices=_selections['genders'],
         min_ratio=0.3,
         case_sensitive=False,
-        mode='approx'
+        mode='approx',
     )),
-    MF("wwid", cp.Integer),
-    MF("email_given"),
-    MF("job_title"),
-    MF("department_self"),
-    MF("department_preference", mentee_only=True),
-    MF("location", cellpattern=cp.StringChoice(
-        choices=selections.locations,
+    MentoringField("location", cellpattern=cp.StringChoice(
+        choices=_selections['location'],
         min_ratio=0.3,
         case_sensitive=False,
-        mode='approx'
+        mode='approx',
     )),
-    MF("years_total", cp.Float),
-    MF("years_jnj", cp.Float),
-    MF("position_level", cp.Digit),
-    MF("preferred_wwids", cp.IntegerList, mentee_only=True),
-    MF("max_mentee_count", mentor_only=True, cellpattern=cp.StringChoice(
+
+
+    # SKILLS
+    MentoringField("skills", cellpattern=cp.StringChoiceMulti(
+            choices=_selections['skill'],
+            case_sensitive=False,
+    )),
+
+
+    # FUNCTION
+    MentoringField('function', cellpattern=cp.StringChoice(
+            choices=_selections['functions'],
+            case_sensitive=False,
+            mode='approx',
+    )),
+    MentoringField(
+        name='preferred_functions', cellpattern=cp.StringChoiceMulti(
+            choices=_selections['functions'],
+            case_sensitive=False,
+    )),
+
+
+    # PREFERENCES
+    MentoringField("preferred_wwids", cp.IntegerList, mentee_only=True),
+    MentoringField(
+        name="max_mentee_count",
+        mentor_only=True,
+        cellpattern=cp.StringChoice(
             dict_use_keys=False,
             mode='approx',
-            choices=selections.yesnomaybe),
+            choices=_selections['yesnomaybe']
+        ),
     ),
 ]
 
@@ -105,33 +109,23 @@ _fieldschema = [
 # YES/MAYBE/NO QUESTIONS #
 #  (mentor and mentee)   #
 ##########################
-for item in selections.locations + selections.genders:
-    _fieldschema.append(MF(name=item, cellpattern=cp.StringChoice(
-        choices=selections.yesnomaybe,
+for item in _selections['locations'] + _selections['gender']:
+    _fieldschemas.append(MentoringField(name=item, cellpattern=cp.StringChoice(
+        choices=_selections['yesnomaybe'],
         dict_use_keys=False,
         default='no',
         case_sensitive=False,
         mode='approx'
     )))
 
-#################
-# MENTOR SKILLS #
-# (mentee only) #
-#################
-_fieldschema.append(MF(
-    name="mentor_skills",
-    cellpattern=cp.StringChoiceMulti(
-        choices=selections.mentor_skills,
-        case_sensitive=False,
-    ),
-    mentee_only=True,
-))
 
-
-fieldschemas = {
-    "mentors": [field for field in _fieldschema if not field.mentee_only],
-    "mentees": [field for field in _fieldschema if not field.mentor_only],
-}
+fieldschemas = {}
+for applicant_type in ApplicantType:
+    fieldschemas[applicant_type] = [
+        field
+        for field in _fieldschemas
+        if field.applicable_to(applicant_type)
+    ]
 
 # This is separate sheet in the excel workbook
 # A mentee on this list is someone we really want to get paired this year.
@@ -142,5 +136,5 @@ favor = [
 ]
 
 
-MentoringField.check_for_unused_toml_fields()
-selections.check_for_unused_toml_fields()
+# MentoringField.check_for_unused_toml_fields()
+# selections.check_for_unused_toml_fields()
